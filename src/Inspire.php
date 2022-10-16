2e7e25ddb6f640cb5eea79c0c5c7e2519bf70e36
ItsMurumba <kevmurumba@gmail.com>
2022-10-16 14:36:03 -0400

    Bug Fix on namespace

--- src/Inspire.php
+++ src/Inspire.php
@@ -5,7 +5,7 @@ namespace Itsmurumba\Inspire;
 use Illuminate\Support\Facades\Http;
 
 class Inspire {
-    
+
     public function justDoIt(){
         $response = Http::get('https://inspiration.goprogram.ai/');
 
